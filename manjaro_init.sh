e01fdd699df8f4e6d9df8be29a6407bc8240c379
cabbage7th <hellocabbage@qq.com>
2020-10-08 19:29:20 +0800

    # 请为您的变更输入提交说明。以 '#' 开始的行将被忽略，而一个空的提交# 说明将会终止提交。## 位于分支 master## 初始提交## 要提交的变更：	新文件：   manjaro_init.sh## 未跟踪的文件:#	test#

---
+++ manjaro_init.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# select a mirror in China
+sudo pacman-mirrors -i -c China -m rank
+# update cache
+sudo pacman -Syy
+# install yay
+sudo pacman -S yay
+
+# install base libs
+yay -S base-devel
+# install base tools
+yay -S git vim make cmake gcc tmux
+
+# sugou pinyin
+yay -S fcitx-im fcitx-configtool fcitx-sogoupinyin
+echo "export GTK_IM_MODULE=fcitx" > ~/.xprofile
+echo "export QT_IM_MODULE=fcitx" >> ~/.xprofile
+echo 'export XMODIFIERS="@im=fcitx"' >> ~/.xprofile
+
+# install softwares
+yay -S google-chrome
+yay -S wps-offce ttf-wps-fonts
+yay -S visual-studio-code-bin
+
+
